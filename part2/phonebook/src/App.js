23f97d757880b03024110ca558afae7484833df9
johnnythrash <chris.garofolo@gmail.com>
2021-05-23 08:48:53 -0400

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', key: 'Arto Hellas', number: "123-456-789" }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState(' ')
+  const [ showAll, setShowAll ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    
+ 
     const nameObject = {
       name: newName,
       key: newName,
@@ -23,19 +27,18 @@ const App = () => {
  
   }
   
-  const handleNameChange = (event) => {
-  
-   setNewName(event.target.value)
-  }
+  const handleNameChange = (event) =>  setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setShowAll(event.target.value)
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
+  const namesToShow = showAll.length < 1? persons: persons.filter(name=>name.name.toUpperCase() === showAll.toUpperCase())
 
   return (
     <div>
-      <div>debug: {newName}</div>
+      <div>
+        filter <input value={showAll} onChange={handleFilterChange} />
+
+      </div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         
@@ -51,7 +54,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(name=>
+        {namesToShow.map(name=>
           <li key={name.key}>Name:{name.name} {name.number}</li>
         )}
       </ul>
