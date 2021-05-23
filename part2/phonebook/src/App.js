c5bc0f804c798c355e5d987ba852cba9d519a021
johnnythrash <chris.garofolo@gmail.com>
2021-05-23 07:38:07 -0400

    2.7

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', key: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+
+  const addName = (event) => {
+    event.preventDefault()
+    
+    const nameObject = {
+      name: newName,
+      key: newName
+    }
+    
+    if (persons.some(name=> name.name === newName )){
+      alert(`${newName} is already in phonebook!`)
+    } else { setPersons(persons.concat(nameObject)) }
+  
+ 
+  }
+  
+  
+
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        
+        <div>
+          name: <input value={newName}  onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(name=>
+          <li key={name.key}>{name.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
