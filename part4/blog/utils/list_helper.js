7c2247d7ea7adf9e234e74c3c5545d7ff9ef2bc2
johnnythrash <chris.garofolo@gmail.com>
2021-06-04 07:16:14 -0400

    4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,17 @@
-const dummy = (blogs) => {
-	return 1
+
+	const dummy = (blogs) => {
+		return 1
+	}
+	
+	
+const totalLikes = (blogs) => {
+	
+	return blogs.length === 0
+	? 0
+	: blogs.map(blog => blog.likes).reduce((a,b)=> a+b)
 }
 
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
