353d818a85c8b134f706dfac4763a19343dd31a2
johnnythrash <chris.garofolo@gmail.com>
2021-06-04 07:41:19 -0400

    4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -11,7 +11,15 @@ const totalLikes = (blogs) => {
 	: blogs.map(blog => blog.likes).reduce((a,b)=> a+b)
 }
 
+
+const favoriteBlog = (blogs) => {
+	const favorite = blogs.reduce((max,blog)=> max.likes > blog.likes ? max : blog)
+	return favorite
+}
+
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
